b232b31a115b0f79564f0ad09fe25e8116d76a55
Saad Naeem <jsaad3663@gmail.com>
2024-07-10 18:59:23 +0500

    Change the title

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,14 +8,14 @@ export default function Navbar() {
     return (
         <nav className={`${theme} w-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 shadow-md`}>
             <div className="container mx-auto flex justify-between items-center p-6">
-                <div className="text-white text-2xl font-bold">Todo App</div>
-                <button
-                    className="text-white flex items-center justify-center w-10 h-10 rounded-full bg-gray-800 hover:bg-gray-700 transition duration-300"
-                    onClick={toggleTheme}
-                >
-                    {theme === "dark" ? <FaSun /> : <FaMoon />}
-                </button>
-            </div>
+                <div className="text-white text-2xl font-bold">Next Todo
+                    <button
+                        className="text-white flex items-center justify-center w-10 h-10 rounded-full bg-gray-800 hover:bg-gray-700 transition duration-300"
+                        onClick={toggleTheme}
+                    >
+                        {theme === "dark" ? <FaSun /> : <FaMoon />}
+                    </button>
+                </div>
         </nav>
     );
 }
